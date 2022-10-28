31a617be4d824841b8087b1b6b7e26ae80363733
pritaelisabethlauratarigan <pritatarigan01@gmail.com>
2022-10-29 04:23:18 +0700

    ajax done

--- cal/urls.py
+++ cal/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('',views.CalendarView.as_view(), name='calendar'),
     path('event/new/',views.event, name='event_new'),
     path('event/edit/<int:event_id>',views.event, name='event_edit'),
+    path('event/new-post/',views.event_post, name='event_new_post'),
 
     ]
