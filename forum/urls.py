61db621855d3a03f5b65b63491ad9591b71a8127
nfmalano <nfmalano@gmail.com>
2022-10-28 14:50:04 +0700

    Creating forum and comments, add delete feature

--- forum/urls.py
+++ forum/urls.py
@@ -5,10 +5,11 @@ app_name = 'forum'
 
 urlpatterns = [
     path('', index, name='Forum'),
-    path('postToForum/',post_to_forum,name='postToForum'),
     path('detail/<int:id>/', forum_post_detail, name="detail"),
     path('api/ForumHome/', get_forum_list, name="getForumList"),
     path('api/addForum/', create_post_ajax, name="addNewForum"),
     path('api/comment/<int:id>/', get_comment_list, name="getCommentList"),
-    path('api/addComment/<int:id>', create_comment_ajax, name="addNewComment")
+    path('api/addComment/<int:id>/', create_comment_ajax, name="addNewComment"),
+    path('deleteForum/<int:id>/', delete_forum, name="deleteForum"),
+    path('deleteComment/<int:id>/', delete_comment, name="deleteComment")
 ]
