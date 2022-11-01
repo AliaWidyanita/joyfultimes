f86db610f6de41c183daea66c084897235a3a6b7
Alia Widyanita Puspaningrum <alia.widyanita@ui.ac.id>
2022-11-01 07:13:52 +0700

    Fixed assessment views.py

--- assessment/models.py
+++ assessment/models.py
@@ -6,4 +6,5 @@ from django.contrib.auth.models import User
 class MentalHealthAssessment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now=True)
+    topic = models.IntegerField(default=0)
     result = models.CharField(max_length=30)
