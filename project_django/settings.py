001af532620edc5031b27e01a8d9b7a14eed428a
Naznien Fevrianne Malano <111237114+naznienfevrianne@users.noreply.github.com>
2022-12-04 16:46:05 +0700

    Fixing csrf

--- project_django/settings.py
+++ project_django/settings.py
@@ -38,6 +38,7 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CSRF_TRUSTED_ORIGINS = ['https://joyfultimes.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
